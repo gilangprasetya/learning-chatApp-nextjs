22eab766908f33dbc8fcd13b2dc573bf71272e79
Gilang pRasetya <gilangprasetya9@gmail.com>
2023-09-13 14:20:02 +0700

    css dikit ga ngaruh

--- app/components/Chat/ChatItem.tsx
+++ app/components/Chat/ChatItem.tsx
@@ -6,7 +6,7 @@ export const ChatItem = ({ message }: { message: Message }) => {
 
     const user = JSON.parse(localStorage.getItem('user') || 'null')
     return (
-        <div className={message.sender === user.username ? styles.messageLeft : styles.messageRight}>
+        <div className={message.sender === user.username ? styles.messageRight : styles.messageLeft}>
             <p>{message.content}</p>
         </div>
     )
